90afd7ecfb6021183ad13590afa411336c4a984d
André Rocha <andrenakarocha@hotmail.com>
2024-10-10 17:34:25 -0300

    Home Cards add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Hero />
+      <HomeCards />
     </>
   )
 }
